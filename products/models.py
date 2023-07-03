195238a31847749e02f4b4baadca5756ea06ade9
femi <Femi.Olulana@liontechacademy.com>
2023-07-03 01:08:07 -0400

    added featured field to product model 2

--- products/models.py
+++ products/models.py
@@ -7,4 +7,4 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    featured = models.BooleanField()
+    featured = models.BooleanField(default=True)
