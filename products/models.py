45648629f02dc3cf419f6027aa767aae4b4e616e
femi <Femi.Olulana@liontechacademy.com>
2023-07-03 00:47:49 -0400

    update product model

--- products/models.py
+++ products/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.TextField()
-    description = models.CharField(max_length=15)
-    price = models.TextField()
-    summary = models.TextField(default="summary")
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
