267223279f8ddbe65e021a9fdfb7a4a124e11015
femi <Femi.Olulana@liontechacademy.com>
2023-07-03 01:06:46 -0400

    added featured field to product model

--- products/models.py
+++ products/models.py
@@ -5,6 +5,6 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    featured = models.BooleanField()
