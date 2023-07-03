1c2a441ecedf2dbd6f7dc3c08b13854db59d56e3
femi <Femi.Olulana@liontechacademy.com>
2023-07-03 00:28:19 -0400

    added max length to CharField

--- products/models.py
+++ products/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     title = models.TextField()
-    description = models.CharField()
+    description = models.CharField(max_length=15)
     price = models.TextField()
